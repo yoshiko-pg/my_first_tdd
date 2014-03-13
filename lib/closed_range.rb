13a1177a433b388de62b8ca431d88309bd3a72fa
maasa <maasa.giss2@gmail.com>
2014-03-14 00:56:32 +0900

    インデントをタブに変更

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -1,26 +1,26 @@
 class ClosedRange
 
-  def initialize(min, max)
-    
-    raise ArgumentError, '引数が整数型でない' if min == 'a'
-    raise ArgumentError, '下端点より上端点が小さい' if max < min
+	def initialize(min, max)
+		
+		raise ArgumentError, '引数が整数型でない' if min == 'a'
+		raise ArgumentError, '下端点より上端点が小さい' if max < min
 
 
-    @min = min
-    @max = max
+		@min = min
+		@max = max
 
-  end
+	end
 
-  def getLowerEndPoint
-    @min
-  end
+	def getLowerEndPoint
+		@min
+	end
 
-  def getUpperEndPoint
-    @max
-  end
+	def getUpperEndPoint
+		@max
+	end
 
-  def toString
-    "[#{@min},#{@max}]"
-  end
+	def toString
+		"[#{@min},#{@max}]"
+	end
 
 end
